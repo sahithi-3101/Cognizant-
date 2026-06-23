0c800182ffe365e9b994c80d0c9792ffcf6b708f
sahithi-3101 <mallampatisahithi@gmail.com>
2026-06-23 09:21:20 +0530

    Implement Singleton Design Pattern using C#

---
+++ DeepSkilling/Design Patterns and Principles/Exercise-01-Singleton-Pattern/Logger.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace SingletonPatternExample
+{
+    public sealed class Logger
+    {
+        private static Logger? instance = null;
+
+        private Logger()
+        {
+            Console.WriteLine("Logger Instance Created");
+        }
+
+        public static Logger GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+
+            return instance;
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"[LOG]: {message}");
+        }
+    }
+}
---
+++ DeepSkilling/Design Patterns and Principles/Exercise-01-Singleton-Pattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SingletonPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger1 = Logger.GetInstance();
+            logger1.Log("Application Started");
+
+            Logger logger2 = Logger.GetInstance();
+            logger2.Log("Processing Data");
+
+            Console.WriteLine();
+
+            if (ReferenceEquals(logger1, logger2))
+            {
+                Console.WriteLine("Singleton Pattern Verified.");
+                Console.WriteLine("Both logger objects refer to the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton Pattern Failed.");
+            }
+        }
+    }
+}
